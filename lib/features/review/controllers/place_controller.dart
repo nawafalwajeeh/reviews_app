519111b355e2d00e39ca33fe291ff486d8ceb75c
= <=>
2025-10-29 19:37:43 +0300

    new modifications

--- lib/features/review/controllers/place_controller.dart
+++ lib/features/review/controllers/place_controller.dart
@@ -70,7 +70,7 @@ class PlaceController extends GetxController {
         'Spa',
         'Gym',
       ],
-      pricePerNight: 450.00,
+      id: '1',
     ),
     PlaceModel(
       thumbnail:
@@ -95,57 +95,7 @@ class PlaceController extends GetxController {
         'Spa',
         'Gym',
       ],
-      pricePerNight: 450.00,
-    ),
-    PlaceModel(
-      thumbnail:
-          'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-      images: [
-        'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-        'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-        'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-      ],
-      title: 'Tropical Paradise Resort',
-      location: 'Maldives, Indian Ocean',
-      category: 'Resort',
-      description:
-          'Experience the serene beauty of the Maldives with our overwater bungalows and crystal-clear lagoon access. Perfect for honeymooners and families looking for a luxury escape. This detailed description helps guests understand the unique offerings and atmosphere of the destination.',
-      rating: 4.8,
-      isFavorite: true,
-      amenities: [
-        'Free Wi-Fi',
-        'Swimming Pool',
-        'Beach Access',
-        'Restaurant',
-        'Spa',
-        'Gym',
-      ],
-      pricePerNight: 450.00,
-    ),
-    PlaceModel(
-      thumbnail:
-          'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-      images: [
-        'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-        'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-        'https://images.unsplash.com/photo-1464979681340-bdd28a61699e?auto=format&fit=crop&w=800&q=80',
-      ],
-      title: 'Tropical Paradise Resort',
-      location: 'Maldives, Indian Ocean',
-      category: 'Resort',
-      description:
-          'Experience the serene beauty of the Maldives with our overwater bungalows and crystal-clear lagoon access. Perfect for honeymooners and families looking for a luxury escape. This detailed description helps guests understand the unique offerings and atmosphere of the destination.',
-      rating: 4.8,
-      isFavorite: true,
-      amenities: [
-        'Free Wi-Fi',
-        'Swimming Pool',
-        'Beach Access',
-        'Restaurant',
-        'Spa',
-        'Gym',
-      ],
-      pricePerNight: 450.00,
+      id: '2',
     ),
   ];
 
@@ -158,3 +108,105 @@ class PlaceController extends GetxController {
     'Park',
   ];
 }
+
+/*
+import 'package:get/get.dart';
+import 'package:shop_app/data/repositories/product/product_repository.dart';
+import 'package:shop_app/features/shop/models/product_model.dart';
+import 'package:shop_app/utils/constants/enums.dart';
+import 'package:shop_app/utils/popups/loaders.dart';
+
+class ProductController extends GetxController {
+  static ProductController get instance => Get.find();
+
+  /// Variables
+  final isLoading = false.obs;
+  final productRepository = Get.put(ProductRepository());
+  RxList<ProductModel> featuredProducts = <ProductModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchFeaturedProducts();
+  }
+
+  Future<void> fetchFeaturedProducts() async {
+    try {
+      // Show loader while loading products
+      isLoading.value = true;
+
+      // Fetch Products
+      final products = await productRepository.getFeaturedProducts();
+
+      // Assign Products to the list
+      featuredProducts.assignAll(products);
+    } catch (e) {
+      AppLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<List<ProductModel>> fetchAllFeaturedProducts() async {
+    try {
+      // Fetch Products
+      final products = await productRepository.getAllFeaturedProducts();
+      return products;
+    } catch (e) {
+      AppLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+      return [];
+    }
+  }
+
+  /// Get Product Price or price range for variations.
+  String getProductPrice(ProductModel product) {
+    double smallestPrice = double.infinity;
+    double largestPrice = 0.0;
+
+    // if no variations exist, return the simple price or sale price.
+    if (product.productType == ProductType.single.toString()) {
+      return (product.salePrice > 0 ? product.salePrice : product.price)
+          .toString();
+    } else {
+      // Calculate the smallest and largest prices among variations
+      for (var variation in product.productVariations!) {
+        // Determine the price to consider (salePrice if available, otherwise regular price)
+        double priceToConsider = variation.salePrice > 0.0
+            ? variation.salePrice
+            : variation.price;
+
+        // Update smallest and largest prices
+        if (priceToConsider < smallestPrice) {
+          smallestPrice = priceToConsider;
+        }
+
+        if (priceToConsider > largestPrice) {
+          largestPrice = priceToConsider;
+        }
+      }
+    }
+
+    // If smallest and largest prices are the same, reaturn a single price
+    if (smallestPrice.isEqual(largestPrice)) {
+      return largestPrice.toString();
+    } else {
+      // Otherwise, return a price range
+      return '$smallestPrice -\$$largestPrice';
+    }
+  }
+
+  /// Calculate Discount Percentage
+  String? calculateSalePercentage(double originalPrice, double? salePrice) {
+    if (salePrice == null || salePrice <= 0.0) return null;
+    if (originalPrice <= 0) return null;
+
+    double percentage = ((originalPrice - salePrice) / originalPrice) * 100;
+    return percentage.toStringAsFixed(0);
+  }
+
+  /// Check Product Stock Status
+  String getProductStockStatus(int stock) {
+    return stock > 0 ? 'In Stock' : 'Out of Stock';
+  }
+}
+*/
--- lib/features/review/models/place_model.dart
+++ lib/features/review/models/place_model.dart
@@ -1,47 +1,154 @@
-// // Demo data model and list
-// class Place {
-//   final String imageUrl;
-//   final String title;
-//   final String location;
-//   final String category;
-//   final String subtitle;
-//   final double rating;
-//   final bool isFavorite;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-//   Place({
-//     required this.imageUrl,
-//     required this.title,
-//     required this.location,
-//     required this.category,
-//     required this.subtitle,
-//     required this.rating,
-//     this.isFavorite = false,
-//   });
-// }
-
-// Place Model
 class PlaceModel {
-  final String thumbnail;
-  final List<String> images;
+  final String id;
   final String title;
+  final String description;
+  final String thumbnail;
+  final List<String>? images;
+  final bool? isFeatured;
+  final DateTime? dateAdded;
   final String location;
   final String category;
-  final String description;
   final double rating;
-  final bool isFavorite;
   final List<String> amenities;
-  final double pricePerNight;
+  final double? latitude;
+  final double? longitude;
+  final String? websiteUrl;
+  bool isFavorite;
 
   PlaceModel({
-    required this.thumbnail,
-    required this.images,
+    required this.id,
     required this.title,
     required this.location,
     required this.category,
     required this.description,
     required this.rating,
-    this.isFavorite = false,
+    required this.thumbnail,
     required this.amenities,
-    required this.pricePerNight,
-  });
+    this.images,
+    bool? isFavorite,
+    this.isFeatured,
+    this.dateAdded,
+    this.latitude,
+    this.longitude,
+    this.websiteUrl,
+  }) : isFavorite = isFavorite ?? false;
+
+  /// -- Create Empty func for clean code
+  static PlaceModel empty() => PlaceModel(
+    id: '',
+    title: '',
+    location: '',
+    category: '',
+    description: '',
+    rating: 0.0,
+    thumbnail: '',
+    amenities: [],
+  );
+
+  /// -- Json Format (To save data to Firestore)
+  Map<String, dynamic> toJson() {
+    return {
+      'Title': title,
+      'Description': description,
+      'Location': location,
+      'Category': category,
+      'Rating': rating,
+      'Thumbnail': thumbnail,
+      'Images': images ?? [],
+      'Amenities': amenities,
+      'IsFeatured': isFeatured ?? false,
+      'DateAdded': dateAdded,
+      'Latitude': latitude,
+      'Longitude': longitude,
+      'WebsiteUrl': websiteUrl,
+    };
+  }
+
+  /// -- Map Json oriented document snapshot from Firebase to Model
+  factory PlaceModel.fromSnapshot(
+    DocumentSnapshot<Map<String, dynamic>> document,
+  ) {
+    if (document.data() == null) return PlaceModel.empty();
+    final data = document.data()!;
+
+    return PlaceModel(
+      id: document.id,
+      title: data['Title'] ?? '',
+      description: data['Description'] ?? '',
+      location: data['Location'] ?? '',
+      category: data['Category'] ?? '',
+      rating: double.parse((data['Rating'] ?? 0.0).toString()),
+      thumbnail: data['Thumbnail'] ?? '',
+      isFeatured: data['IsFeatured'] ?? false,
+      dateAdded: data['DateAdded'] != null
+          ? (data['DateAdded'] as Timestamp).toDate()
+          : null,
+      latitude: double.tryParse((data['Latitude'] ?? 0.0).toString()),
+      longitude: double.tryParse((data['Longitude'] ?? 0.0).toString()),
+      websiteUrl: data['WebsiteUrl'],
+      images: data['Images'] != null ? List<String>.from(data['Images']) : [],
+      amenities: data['Amenities'] != null
+          ? List<String>.from(data['Amenities'])
+          : [],
+      isFavorite: false,
+    );
+  }
+
+  /// -- Map Json-oriented document snapshot from Query results (Fully Coded)
+  factory PlaceModel.fromQuerySnapshot(
+    QueryDocumentSnapshot<Object?> document,
+  ) {
+    final data = document.data() as Map<String, dynamic>;
+
+    return PlaceModel(
+      id: document.id,
+      title: data['Title'] ?? '',
+      description: data['Description'] ?? '',
+      location: data['Location'] ?? '',
+      category: data['Category'] ?? '',
+      rating: double.parse((data['Rating'] ?? 0.0).toString()),
+      thumbnail: data['Thumbnail'] ?? '',
+      isFeatured: data['IsFeatured'] ?? false,
+      dateAdded: data['DateAdded'] != null
+          ? (data['DateAdded'] as Timestamp).toDate()
+          : null,
+      latitude: double.tryParse((data['Latitude'] ?? 0.0).toString()),
+      longitude: double.tryParse((data['Longitude'] ?? 0.0).toString()),
+      websiteUrl: data['WebsiteUrl'],
+      images: data['Images'] != null ? List<String>.from(data['Images']) : [],
+      amenities: data['Amenities'] != null
+          ? List<String>.from(data['Amenities'])
+          : [],
+      isFavorite: false,
+    );
+  }
 }
+
+//----------------------
+// class PlaceModel {
+//   final String thumbnail;
+//   final List<String> images;
+//   final String title;
+//   final String location;
+//   final String category;
+//   final String description;
+//   final double rating;
+//   final bool isFavorite;
+//   final List<String> amenities;
+//   final double pricePerNight;
+
+//   PlaceModel({
+//     required this.thumbnail,
+//     required this.images,
+//     required this.title,
+//     required this.location,
+//     required this.category,
+//     required this.description,
+//     required this.rating,
+//     this.isFavorite = false,
+//     required this.amenities,
+//     required this.pricePerNight,
+//   });
+// }
